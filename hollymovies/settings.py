3e540965cb77370f897fcdd3cab81dd6336fbed6
kacpergit <79944487+kacpergit@users.noreply.github.com>
2021-10-18 10:38:51 +0100

    Update settings.py

--- hollymovies/settings.py
+++ hollymovies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4t)&4=f^jd_gl2xmgtoy$xnlaedl8c5=k6^-vr5#3#jc5*_-u1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
